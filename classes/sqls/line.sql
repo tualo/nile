113ffc45b340d67a01c7e617398d344e2d9d43cb
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2014-09-07 13:09:36 +0200

    reduce geo orperations, for speedup tests

--- classes/sqls/line.sql
+++ classes/sqls/line.sql
@@ -6,38 +6,12 @@ SELECT
     )
     , 0
   ) AS data,
+z_order,
 
 	hstore2json( tags ) as tags,
 	Null AS reprpoint
 FROM
-  (
-  	SELECT
-      (
-        ST_Dump(
-          ST_Multi(
-            ST_SimplifyPreserveTopology(
-              ST_LineMerge(##way_column), ##buffer
-            )
-          )
-        )
-      ).geom AS ##way_column,
-
-      tags
-  	FROM
-  		(
-  			SELECT
-          ST_Union(##way_column) AS ##way_column,
-
-
-          tags
-  			FROM
-          ##prefix_line
-  			WHERE
-          ST_Intersects(  ##way_column, ##srid )
-          ##cond
-  			GROUP BY
-
-
-          tags
-  		) p
-  ) p
+  ##prefix_line
+WHERE
+  ST_Intersects(  ##way_column, ##srid )
+  ##cond
