8ec2846bc2ee6685f09af364309c17e67db93146
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2014-10-14 19:37:51 +0200

    removed older code

--- classes/Geocoder.js
+++ classes/Geocoder.js
@@ -19,17 +19,16 @@ var Geocoder = function(system,debug){
 
 Geocoder.prototype.geoCode = function(city,street,callback){
 
-city = encodeURIComponent(city);
-street = encodeURIComponent(street);
+  city = encodeURIComponent(city);
+  street = encodeURIComponent(street);
 
-var json,
-    self = this,
-    data = '',
-    url = "http://"+self.system.config.geocode.host+':'+self.system.config.geocode.port+'/'+ (self.system.config.geocode.path.replace('{city}', city).replace('{street}', street)),
-    req;
-  ///url = url.replace('{address}', address.replace(/\s/g,'%20'));
+  var json,
+  self = this,
+  data = '',
+  url = "http://"+self.system.config.geocode.host+':'+self.system.config.geocode.port+'/'+ (self.system.config.geocode.path.replace('{city}', city).replace('{street}', street)),
+  req;
 
-  console.log(url,http.headers);
+
   http.get(url,function(res){
     res.setEncoding('utf8');
     res.on('data', function (chunk) {
@@ -45,44 +44,6 @@ var json,
     });
   })
 
-/*
-  req = http.request(options, function(res) {
-    //console.log('STATUS: ' + res.statusCode);
-    //console.log('HEADERS: ' + JSON.stringify(res.headers));
-
-  });
-
-  req.on('error', function(e) {
-    console.log('problem with request: ' + e.message);
-    callback(e);
-  });
-
-  // write data to request body
-  //  req.write('data\n');
-  //  req.write('data\n');
-  req.end();
-*/
-/*
-  var child,
-  self = this,
-  json;
-
-  child = exec(self.system.config.nominatim+' --search "'+address+'"', function (error, stdout, stderr) {
-    if (self.debug){
-      console.log('exec: ',error,stdout,stderr);
-    }
-    if (error){
-      callback(error);
-    }else{
-      try{
-        json = JSON.parse(stdout);
-        callback(null,json);
-      }catch(e){
-        callback(e);
-      }
-    }
-  });
-*/
 }
 
 
