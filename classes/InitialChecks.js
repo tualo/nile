7f0f4f64774b4fed942ed7b2abd5b375dbfd8289
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2014-10-17 15:24:22 +0200

    added basic setup functions

--- classes/InitialChecks.js
+++ classes/InitialChecks.js
@@ -8,16 +8,46 @@ var InitialChecks = function(system,debug){
   self.system = system;
   self.functions;
   self.extensions;
+  self.tables;
+}
+
+InitialChecks.prototype.checkTable = function(name,callback){
+  var self = this,
+  client = self.system.client,
+  sql = [
+  'SELECT c.relname ',
+  '    FROM   pg_catalog.pg_class c',
+  '    JOIN   pg_catalog.pg_namespace n ON n.oid = c.relnamespace',
+  '    WHERE  n.nspname = current_schema()'
+  ].join(' ');
+  name = name.toLowerCase();
+  if (typeof self.tables!=='undefined'){
+    callback(null,self.tables.indexOf(name) > -1);
+  }else{
+    self.tables = [];
+    client.query( sql , function(err, results){
+
+      if (err){
+        self.system.logger.log('error',err);
+        //callback(err, null);
+        callback(err,false);
+      }else{
+        for (i=0,m=results.rows.length;i<m;i++){
+          self.tables.push(results.rows[i].relname);
+        }
+        callback(null,self.tables.indexOf(name) > -1);
+      }
+    });
+  }
 }
 
 
-
-InitialChecks.prototype.checkExtensions = function(name,callback){
+InitialChecks.prototype.checkExtension = function(name,callback){
   var self = this,
-      client = self.system.client,
-      sql = [
-        'select name from pg_available_extensions()'
-      ].join(' ');
+  client = self.system.client,
+  sql = [
+  'select name from pg_available_extensions()'
+  ].join(' ');
   name = name.toLowerCase();
   if (typeof self.extensions!=='undefined'){
     callback(null,self.extensions.indexOf(name) > -1);
@@ -25,32 +55,32 @@ InitialChecks.prototype.checkExtensions = function(name,callback){
     self.extensions = [];
     client.query( sql , function(err, results){
 
-        if (err){
-          self.system.logger.log('error',err);
-          //callback(err, null);
-          callback(err,false);
-        }else{
-          for (i=0,m=results.rows.length;i<m;i++){
-            self.extensions.push(results.rows[i].name);
-          }
-          callback(null,self.extensions.indexOf(name) > -1);
+      if (err){
+        self.system.logger.log('error',err);
+        //callback(err, null);
+        callback(err,false);
+      }else{
+        for (i=0,m=results.rows.length;i<m;i++){
+          self.extensions.push(results.rows[i].name);
         }
+        callback(null,self.extensions.indexOf(name) > -1);
+      }
     });
   }
 }
 
 InitialChecks.prototype.checkFunction = function(name,callback){
   var self = this,
-      client = self.system.client,
-      sql = [
-        'SELECT format_type(p.prorettype, NULL) as "Result",',
-        'lower(p.proname) as "Function",',
-        'oidvectortypes(p.proargtypes) as "Arguments"',
-        'FROM pg_proc p',
-        'WHERE p.prorettype <> 0 and (pronargs = 0 or',
-        'oidvectortypes(p.proargtypes) <> \'\')',
-        'ORDER BY "Function", "Result", "Arguments"'
-      ].join(' ');
+  client = self.system.client,
+  sql = [
+  'SELECT format_type(p.prorettype, NULL) as "Result",',
+  'lower(p.proname) as "Function",',
+  'oidvectortypes(p.proargtypes) as "Arguments"',
+  'FROM pg_proc p',
+  'WHERE p.prorettype <> 0 and (pronargs = 0 or',
+  'oidvectortypes(p.proargtypes) <> \'\')',
+  'ORDER BY "Function", "Result", "Arguments"'
+  ].join(' ');
   name = name.toLowerCase();
   if (typeof self.functions!=='undefined'){
     callback(null,self.functions.indexOf(name) > -1);
@@ -58,16 +88,16 @@ InitialChecks.prototype.checkFunction = function(name,callback){
     self.functions = [];
     client.query( sql , function(err, results){
 
-        if (err){
-          self.system.logger.log('error',err);
-          //callback(err, null);
-          callback(err,false);
-        }else{
-          for (i=0,m=results.rows.length;i<m;i++){
-            self.functions.push(results.rows[i].Function);
-          }
-          callback(null,self.functions.indexOf(name) > -1);
+      if (err){
+        self.system.logger.log('error',err);
+        //callback(err, null);
+        callback(err,false);
+      }else{
+        for (i=0,m=results.rows.length;i<m;i++){
+          self.functions.push(results.rows[i].Function);
         }
+        callback(null,self.functions.indexOf(name) > -1);
+      }
     });
   }
 }
